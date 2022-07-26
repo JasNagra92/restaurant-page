b4fb723de862447005cadb91cdd0b3c3f87b8546
jasmac <jaskarannagra@hotmail.com>
2022-07-25 20:08:59 -0700

    add styling and 3 tabs for navigation"
    "

--- src/pageload.js
+++ src/pageload.js
@@ -3,7 +3,6 @@ import Icon from "../src/frontpage.jpg";
 const pageload = function(){
     const title = document.createElement("h1");
     const content = document.getElementById("content");
-    console.log(content)
     title.textContent = "Nagra Smoke House";
     content.appendChild(title);
 
@@ -18,6 +17,22 @@ const pageload = function(){
     const myIcon = new Image();
     myIcon.src = Icon;
     imgContainer.appendChild(myIcon);
+
+    const navBar = document.createElement('div')
+    navBar.classList.add('nav')
+    content.appendChild(navBar)
+
+    const homeBtn = document.createElement('button')
+    navBar.appendChild(homeBtn)
+    const menuBtn = document.createElement('button')
+    navBar.appendChild(menuBtn)
+    const contactBtn = document.createElement('button')
+    navBar.appendChild(contactBtn)
+    homeBtn.classList.add('navbtn')
+    menuBtn.classList.add('navbtn')
+    contactBtn.classList.add('navbtn')
+
+
 
     const intro = document.createElement('p')
     intro.textContent = "The best smoked shalmon in all of Surrey BC"
