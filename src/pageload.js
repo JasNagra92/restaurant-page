180e93d2f6d6ac00e281f10df8e7ebe84c462ad9
jasmac <jaskarannagra@hotmail.com>
2022-07-25 20:35:45 -0700

    add styling to nav buttons and a review and hours section

--- src/pageload.js
+++ src/pageload.js
@@ -1,5 +1,3 @@
-import Icon from "../src/frontpage.jpg";
-
 const pageload = function(){
     const title = document.createElement("h1");
     const content = document.getElementById("content");
@@ -10,34 +8,32 @@ const pageload = function(){
     mainContent.classList.add("mainContent");
     content.appendChild(mainContent)
 
-    const imgContainer = document.createElement('div');
-    imgContainer.classList.add("imgContainer")
-    mainContent.appendChild(imgContainer)
-
-    const myIcon = new Image();
-    myIcon.src = Icon;
-    imgContainer.appendChild(myIcon);
-
     const navBar = document.createElement('div')
     navBar.classList.add('nav')
     content.appendChild(navBar)
 
     const homeBtn = document.createElement('button')
     navBar.appendChild(homeBtn)
+    homeBtn.innerHTML = "HOME"
     const menuBtn = document.createElement('button')
     navBar.appendChild(menuBtn)
+    menuBtn.innerHTML = "MENU"
     const contactBtn = document.createElement('button')
     navBar.appendChild(contactBtn)
+    contactBtn.innerHTML = "CONTACT"
     homeBtn.classList.add('navbtn')
     menuBtn.classList.add('navbtn')
     contactBtn.classList.add('navbtn')
 
+    const review = document.createElement('p')
+    review.classList.add('review')
+    review.innerHTML = "<em>I had some of the brisket at a kids Birthday party and it was unreal</em> <br> Mr. Ho"
+    mainContent.appendChild(review)
 
-
-    const intro = document.createElement('p')
-    intro.textContent = "The best smoked shalmon in all of Surrey BC"
-    mainContent.appendChild(intro)
-    
+    const hours = document.createElement('p')
+    hours.classList.add('hours')
+    hours.innerHTML = "Monday - Friday <br> 9:00 AM - 5:00 PM <br> Saturday/Sunday <br> Closed"
+    mainContent.appendChild(hours)
 }
 
 export{pageload}
